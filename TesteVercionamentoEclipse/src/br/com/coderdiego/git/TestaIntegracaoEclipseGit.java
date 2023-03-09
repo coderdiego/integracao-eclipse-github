44e76f7e442c9ea2fde1262ec131fe344dcba628
Diegocoderdab <diego.ab.eletricista@gmail.com>
2023-03-09 14:16:34 -0300

    Alteração para testar versionamento

--- TesteVercionamentoEclipse/src/br/com/coderdiego/git/TestaIntegracaoEclipseGit.java
+++ TesteVercionamentoEclipse/src/br/com/coderdiego/git/TestaIntegracaoEclipseGit.java
@@ -10,7 +10,8 @@ package br.com.coderdiego.git;
 public class TestaIntegracaoEclipseGit {
 
 	public static void main(String[] args) {
-		System.out.println("Teste de vercionamento");
+		System.out.println("Teste de versionamento");
+		System.out.println("Primeiro commit feito pela IDE");
 	}
 
 }
