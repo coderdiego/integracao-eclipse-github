ceb717269eff7b7f807efef1990472ce420e2e31
Diegocoderdab <diego.ab.eletricista@gmail.com>
2023-03-09 14:41:51 -0300

    Commit teste versionamento

--- TesteVercionamentoEclipse/src/br/com/coderdiego/git/TestaIntegracaoEclipseGit.java
+++ TesteVercionamentoEclipse/src/br/com/coderdiego/git/TestaIntegracaoEclipseGit.java
@@ -11,7 +11,7 @@ public class TestaIntegracaoEclipseGit {
 
 	public static void main(String[] args) {
 		System.out.println("Teste de versionamento");
-		System.out.println("Primeiro commit feito pela IDE");
+		System.out.println("Commit feito pela IDE");
 	}
 
 }
