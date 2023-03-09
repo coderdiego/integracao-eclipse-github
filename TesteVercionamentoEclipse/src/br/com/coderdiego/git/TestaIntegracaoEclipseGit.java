2c6a53a47e39aa1ab64efdb25a0508e775f92dbb
Diego Araujo <76620437+coderdiego@users.noreply.github.com>
2023-03-09 14:00:54 -0300

    Adicionei os primeiros arquivos via upload

---
+++ TesteVercionamentoEclipse/src/br/com/coderdiego/git/TestaIntegracaoEclipseGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.coderdiego.git;
+
+/**
+ * @author CoderDiego
+ *
+ */
+public class TestaIntegracaoEclipseGit {
+
+	public static void main(String[] args) {
+		System.out.println("Teste de vercionamento");
+	}
+
+}
---
+++ TesteVercionamentoEclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author CoderDiego
+ *
+ */
+module TesteVercionamentoEclipse {
+}
